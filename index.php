ffd1f377c98cf7f6e381beee47ed6a175d63f88b
Tatiana Koroleva <87785060+tatyanakoroleva00@users.noreply.github.com>
2024-10-28 12:48:53 +0300

    'finished'

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ function formattedPrice($arg) {
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?=$elem['category']?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$elem['name']?></a></h3>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.php"><?=$elem['name']?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
